905a9a88c675e7db9f98b87bd0166d588a34f8e7
rence141 <Lorenzezz0987@gmail.com>
2025-10-04 22:27:45 +0800

    change db

--- main/php/db.php
+++ main/php/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "sql12.freesqldatabase.com";
 $user = "sql12801294";
-$pass = "your_password_here";   // get this from your FreeSQLDatabase dashboard
+$pass = "sptMsAVPRz";   // get this from your FreeSQLDatabase dashboard
 $dbname = "sql12801294";        // usually same as username
 $port = 3306;                    // default port for remote MySQL
 
